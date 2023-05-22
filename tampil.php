57a0c8eb4e6daaf741bafbc6029c438355db7235
paulkusuma <118434047+paulkusuma@users.noreply.github.com>
2023-05-23 00:48:45 +0700

    menyelesaikan update
    masih terdapat kesalahan didalam buttom checked

--- tampil.php
+++ tampil.php
@@ -1,34 +1,53 @@
 <?php
 require './connect_mysql.php';
 
-
-
 $sql = "SELECT `id`, `task`, `status` FROM `tb_todolist`";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
-        // echo "id: " . $row["id"] . " - Tugas: " . $row["task"] . " " . $row["status"] . "<br>";
         echo '<ul class="list-group list-group-horizontal rounded-0 bg-transparent">';
         echo '  <li class="list-group-item d-flex align-items-center ps-0 pe-3 py-1 rounded-0 border-0 bg-transparent">';
         echo '    <div class="form-check">';
-        echo '      <input class="form-check-input me-0" type="checkbox" value="false" id="flexCheckChecked1" aria-label="..." checked />';
+        $checked = $row["status"] == 0 ? 'checked' : ''; // menentukan nilai 'checked' berdasarkan status
+        if ($row["status"] == 1) {
+            echo '      <a href="update.php?id=' . $row["id"] . '"><input class="form-check-input me-0" type="checkbox" value="" id="flexCheckChecked1" aria-label="..." ' . $checked . ' /></a>';
+        } else {
+            echo '      <input class="form-check-input me-0" type="checkbox" value="" id="flexCheckChecked1" aria-label="..." ' . $checked . ' />';
+        }
         echo '    </div>';
         echo '  </li>';
+
         echo '  <li class="list-group-item px-3 py-1 d-flex align-items-center flex-grow-1 border-0 bg-transparent">';
         echo '    <p class="lead fw-normal mb-0">' . $row["task"] . '</p>';
         echo '  </li>';
+
         echo '  <li class="list-group-item ps-3 pe-0 py-1 rounded-0 border-0 bg-transparent">';
         echo '    <div class="d-flex flex-row justify-content-end mb-1">';
-        echo '      <a href="update.php?id=' . $row["id"] . '" class="text-info  me-3 " data-mdb-toggle="tooltip" title="Edit todo"><i class="fas fa-pencil-alt"></i></a>';
+        // echo '      <a href="update.php?id=' . $row["id"] . '" class="text-info me-3" data-mdb-toggle="tooltip" title="Edit todo"><i class="fas fa-pencil-alt"></i></a>';
         echo '      <a href="delete.php?id=' . $row["id"] . '" data-mdb-toggle="tooltip" title="Delete todo"><i class="fas fa-trash-alt"></i></a>';
         echo '    </div>';
         echo '  </li>';
         echo '</ul>';
+
     }
 } else {
-    echo "0 results";
+
 }
 $conn->close();
 ?>
+
+<!-- <script>
+    function updateStatus(id, checked) {
+        var status = checked ? 0 : 1;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                // Response handling, if needed
+            }
+        };
+        xhttp.open("GET", "update.php?id=" + id + "&status=" + status, true);
+        xhttp.send();
+    }
+</script> -->
--- update.php
+++ update.php
@@ -3,12 +3,20 @@ require './connect_mysql.php';
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
-    $sql = "UPDATE `tb_todolist` SET task='Sudah terupdate' WHERE id='$id'";
+    $sql = "SELECT `task` FROM `tb_todolist` WHERE `id` = '$id'";
+    $result = $conn->query($sql);
     header('location:toDoList.php');
-    if ($conn->query($sql) === TRUE) {
-        // echo "Record updated successfully";
-    } else {
-        // echo "Error updating record: " . $conn->error;
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $task = $row['task'];
+        $updatedTask = '<del>' . $task . '</del>';
+
+        $updateSql = "UPDATE `tb_todolist` SET `task` = '$updatedTask' WHERE `id` = '$id'";
+        if ($conn->query($updateSql) === TRUE) {
+            // echo "Record updated successfully";
+        } else {
+            // echo "Error updating record: " . $conn->error;
+        }
     }
 }
 $conn->close();
